b594404543f74072704fdd32ed7fc24e466e285a
mrcrvndotcom <rivera.marcraven.bsis@gmail.com>
2026-03-02 22:59:37 +0800

    Add files via upload

---
+++ Area of Circle.py
@@ -0,0 +1,8 @@
+print("------------------------------")
+print(" AREA OF A CIRCLE ")
+print("------------------------------")
+
+RiveraRadius = int(input("Enter the radius: "))
+RiveraArea = 3.14 * (RiveraRadius ** 2)
+
+print(f"The approximate area is: {round(RiveraArea)}")
