aaf4d9d6a2a4fe0db6890d5f9c762510186e5f4a
mrcrvndotcom <rivera.marcraven.bsis@gmail.com>
2026-03-02 22:59:09 +0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,10 @@
+print("------------------------------")
+print(" MULTIPLICATION CALCULATOR ")
+print("------------------------------")
+
+RiveraNum1 = int(input("Enter first number: "))
+RiveraNum2 = int(input("Enter second number: "))
+
+RiveraProduct = RiveraNum1 * RiveraNum2
+
+print(f"Answer: {RiveraProduct}")
