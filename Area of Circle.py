3d08b150082489b113023b10a3647d9eb8c9e128
mrcrvndotcom <rivera.marcraven.bsis@gmail.com>
2026-03-02 23:00:22 +0800

    Delete Area of Circle.py

--- Area of Circle.py
+++
@@ -1,8 +0,0 @@
-print("------------------------------")
-print(" AREA OF A CIRCLE ")
-print("------------------------------")
-
-RiveraRadius = int(input("Enter the radius: "))
-RiveraArea = 3.14 * (RiveraRadius ** 2)
-
-print(f"The approximate area is: {round(RiveraArea)}")
